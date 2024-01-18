a6ad6025f0c5d539dd150a5ca0d876f747e3ae12
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-01-17 23:59:05 -0800

    added navbar styling

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,6 +1,5 @@
-import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from './Navbar.module.css'
 import Link from 'next/link'
 
 
@@ -8,6 +7,19 @@ export default function Navbar() {
     return (
         <>
             <main className={styles.main}>
+                <div className={styles.image}>
+                    <Link href={'/'}>
+                        <Image
+                            src={''}
+                            alt=""
+                        />
+                    </Link>
+                </div>
+                <div className={styles.links}>
+                    <Link className={styles.link} href={'/'}><p className={styles.link}>Web Development</p></Link>
+                    <Link className={styles.link} href={'/designs'}><p className={styles.link}>Design</p></Link>
+                    <Link className={styles.link} href={'/'}><p className={styles.link}>Contact</p></Link>
+                </div>
             </main>
         </>
     )
