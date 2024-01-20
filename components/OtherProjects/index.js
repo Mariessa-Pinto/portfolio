df4e779e5283a73717140bd827fad265b67991ef
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-01-19 20:19:47 -0800

    added link and url

--- components/OtherProjects/index.js
+++ components/OtherProjects/index.js
@@ -4,63 +4,83 @@ import Link from 'next/link'
 
 
 export default function OtherProjects({
-    title1="title",
-    imageURL1="",
-    title2="title",
-    imageURL2="",
-    title3="title",
-    imageURL3=""
+    title1 = "title",
+    imageURL1 = "",
+    link1 = "",
+    title2 = "title",
+    imageURL2 = "",
+    link2 = "",
+    title3 = "title",
+    imageURL3 = "",
+    link3 = ""
 }) {
     return (
         <>
             <main className={styles.main}>
-                <div>
-                    <h2>Other Projects</h2>
-                    <div className={styles.projectButton}>
-                        <Image
-                            src={imageURL1}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                        <h5>{title1}</h5>
-                        <Image
-                            src={''}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                    </div>
-                    <div className={styles.projectButton}>
-                        <Image
-                            src={imageURL2}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                        <h5>{title2}</h5>
-                        <Image
-                            src={''}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                    </div>
-                    <div className={styles.projectButton}>
-                        <Image
-                            src={imageURL3}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                        <h5>{title3}</h5>
-                        <Image
-                            src={''}
-                            alt=''
-                            width={100}
-                            height={50}
-                        />
-                    </div>
+                <div className={styles.alignText}>
+                    <h2 className={styles.header}>Other Projects</h2>
+                </div>
+                <div className={styles.buttons}>
+                    <Link href={link1} className={styles.link}>
+                        <div className={styles.projectButton}>
+                            <div className={styles.leftSide}>
+                                <Image
+                                    src={imageURL1}
+                                    alt=''
+                                    width={256}
+                                    height={60}
+                                />
+                                <h5 className={styles.title}>{title1}</h5>
+                            </div>
+                            <Image
+                                className={styles.image}
+                                src={''}
+                                alt=''
+                                width={72}
+                                height={52}
+                            />
+                        </div>
+                    </Link>
+                    <Link href={link2} className={styles.link}>
+                        <div className={styles.projectButton}>
+                            <div className={styles.leftSide}>
+                                <Image
+                                    src={imageURL2}
+                                    alt=''
+                                    width={256}
+                                    height={60}
+                                />
+                                <h5 className={styles.title}>{title2}</h5>
+                            </div>
+                            <Image
+                                className={styles.image}
+                                src={''}
+                                alt=''
+                                width={72}
+                                height={52}
+                            />
+                        </div>
+                    </Link>
+                    <Link href={link3} className={styles.link}>
+                        <div className={styles.projectButton}>
+                            <div className={styles.leftSide}>
+                                <Image
+                                    src={imageURL3}
+                                    alt=''
+                                    width={256}
+                                    height={60}
+                                />
+                                <h5 className={styles.title}>{title3}</h5>
+                            </div>
+                            <Image
+                                className={styles.image}
+                                src={''}
+                                alt=''
+                                width={72}
+                                height={52}
+                            />
+                        </div>
+                    </Link>
                 </div>
             </main>
         </>
