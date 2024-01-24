970869192eae57e3eeb138f8f8e43d25b909d577
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-01-23 20:35:46 -0800

    added next images and file loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
+const withImages = require('next-images');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withImages({
+  reactStrictMode: true,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp4|mov|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
+});
