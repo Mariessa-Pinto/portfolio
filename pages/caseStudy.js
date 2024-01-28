154da1cc547626e224812d32e5fb6f566f754d7a
Mariessa-Pinto <mpinto13@my.bcit.ca>
2024-01-28 14:25:12 -0800

    added the remaining assets

--- pages/caseStudy.js
+++ pages/caseStudy.js
@@ -384,7 +384,7 @@ export default function CaseStudy() {
                                 </ul>
                             </div>
                         </div>
-                        <div style={{overflow: 'auto', maxHeight: '300px', width: '80vw'}}>
+                        <div style={{overflow: 'auto', maxHeight: '300px', width: '60vw'}}>
                             <pre className={styles.codeContainer}>
                                 <code>
                                     {`
@@ -398,151 +398,144 @@ import { statsEmotions } from '../../data/StatsEmotionData';
 import { getAuth } from 'firebase/auth';
 import { ActivityIndicator } from 'react-native-paper';
 
-                                        const DonutChart = () => {
-                                        const positiveColor = "#FFE5A4"
-                                        const negativeColor = "#8CACDB"
-                                        
-                                        const [data, setData] = useState([])
-                                        const [entries, setEntries] = useState([]);
-                                        const [selectedSlice, setSelectedSlice] = useState(null);
-                                        const newCategories = [];
-                                        const countCategories = {};
+const DonutChart = () => {
+const positiveColor = "#FFE5A4"
+const negativeColor = "#8CACDB"
 
-                                        const newColors = [];
-                                        // const colorIndex = [];
-                                        const [finalColors, setFinalColors] = useState([])
-                                        const [emotionData, setEmotionData] = useState(statsEmotions.emotions)
+const [data, setData] = useState([])
+const [entries, setEntries] = useState([]);
+const [selectedSlice, setSelectedSlice] = useState(null);
+const newCategories = [];
+const countCategories = {};
 
-                                        const [loading, setLoading] = useState(true)
-                                        const auth = getAuth()
+const newColors = [];
+// const colorIndex = [];
+const [finalColors, setFinalColors] = useState([])
+const [emotionData, setEmotionData] = useState(statsEmotions.emotions)
 
-                                        //run get data on page load 
-                                        useEffect(() => {
-                                            console.log("new log")
-                                            const fetchJournalEntries = async () => {
-                                                try {
-                                                   // Use the user's UID when fetching journal entries
-                                                    const username = auth.currentUser ? auth.currentUser.uid : null;
-                                                    if (username) {
-                                                        const journalEntries = await getJournalEntries(username);
-                                                        console.log('Journal Entries:', journalEntries);
-                                                        setEntries(Object.values(journalEntries));
-                                                        processJournalData(Object.values(journalEntries));
-                                                        setLoading(false)
-                                                        console.log({loading})
-                                                    } else {
-                                        console.error('User not authenticated.');
-                                                    }
-                                                } catch (error) {
-                                        console.error('Error fetching journal entries:', error);
-                                        setLoading(false)
-                                        console.log({loading})
-                                                }
-                                            };
-                                        fetchJournalEntries();
-                                        }, [auth.currentUser]);
+const [loading, setLoading] = useState(true)
+const auth = getAuth()
 
-                                        const processJournalData = (journalEntries) => {
-                                        console.log("step 1");
-                                        console.log(journalEntries);
-                                    
-                                        Object.values(journalEntries).forEach((entry) => {
-                                            if (Array.isArray(entry.keywords)) {
-                                                entry.keywords.forEach((keyword) => {
-                                                    emotionData && emotionData.map((e, index) => {
-                                                        if (e.emotion === keyword && e.type === "negative") {
-                                                            newCategories.unshift(e.category);
-                                                            newColors.unshift(negativeColor);
-                                                        } else if (e.emotion === keyword && e.type === "positive") {
-                                                            newCategories.push(e.category);
-                                                            newColors.push(positiveColor);
-                                                        }
-                                                    });
-                                                });
-                                            }
-                                        });
-                                    
-                                        console.log(newCategories);
-                                        console.log(newColors);
-                                        console.log("step 2");
-                                    
-                                        newCategories.forEach((x) => {
-                                            countCategories[x] = (countCategories[x] || 0) + 1;
-                                        });
-                                    
-                                        const indices = newCategories.map((emotion) => newCategories.indexOf(emotion));
-                                        const output = new Set(indices);
-                                        const colorIndex = [...output];
-                                    
-                                        colorIndex.forEach((num) => {
-                                            finalColors.unshift(newColors[num]);
-                                        });
-                                        finalColors.reverse();
-                                        console.log("step 3");
-                                    
-                                        console.log("step 4");
-                                        const totalCategories = newCategories.length;
-                                    
-                                        const chartData = Object.entries(countCategories).map(([keyword, count]) => ({
-                                            x: keyword,
-                                            y: Number(((count / totalCategories)).toFixed(2)),
-                                        }));
-                                        setData(chartData);
-                                        console.log("complete");
-                                    };
-                                    
-                                        const handleSliceClick = (event, props) => {
-                                        setSelectedSlice(selectedSlice === props.index ? null : props.index);
-                                        };
+//run get data on page load 
+useEffect(() => {
+    console.log("new log")
+    const fetchJournalEntries = async () => {
+        try {
+            // Use the user's UID when fetching journal entries
+            const username = auth.currentUser ? auth.currentUser.uid : null;
+            if (username) {
+                const journalEntries = await getJournalEntries(username);
+                console.log('Journal Entries:', journalEntries);
+                setEntries(Object.values(journalEntries));
+                processJournalData(Object.values(journalEntries));
+                setLoading(false)
+                console.log({loading})
+            } else {
+                console.error('User not authenticated.');
+            }
+        } catch (error) {
+            console.error('Error fetching journal entries:', error);
+            setLoading(false)
+            console.log({loading})
+        }
+    };
+    fetchJournalEntries();
+}, [auth.currentUser]);
 
-                                    //Label Font. This is connected to the app.json "font" object.
-                                    const [fontsLoaded] = useFonts({
-                                        Lexend_400Regular,
-                                    });
+const processJournalData = (journalEntries) => {
+    console.log("step 1");
+    console.log(journalEntries);
+    Object.values(journalEntries).forEach((entry) => {
+        if (Array.isArray(entry.keywords)) {
+            entry.keywords.forEach((keyword) => {
+                emotionData && emotionData.map((e, index) => {
+                    if (e.emotion === keyword && e.type === "negative") {
+                        newCategories.unshift(e.category);
+                        newColors.unshift(negativeColor);
+                    } else if (e.emotion === keyword && e.type === "positive") {
+                        newCategories.push(e.category);
+                        newColors.push(positiveColor);
+                    }
+                });
+            });
+        }
+    });
+    console.log(newCategories);
+    console.log(newColors);
+    console.log("step 2");
+    newCategories.forEach((x) => {
+        countCategories[x] = (countCategories[x] || 0) + 1;
+    });
+    const indices = newCategories.map((emotion) => newCategories.indexOf(emotion));
+    const output = new Set(indices);
+    const colorIndex = [...output];
+    colorIndex.forEach((num) => {
+        finalColors.unshift(newColors[num]);
+    });
+    finalColors.reverse();
+    console.log("step 3");
+    console.log("step 4");
+    const totalCategories = newCategories.length;
+    const chartData = Object.entries(countCategories).map(([keyword, count]) => ({
+        x: keyword,
+        y: Number(((count / totalCategories)).toFixed(2)),
+    }));
+    setData(chartData);
+    console.log("complete");
+    };
 
-                                    return (
-                                    <View style={[styles.container, globalStyles.labelText]}>
-                                        {
-                                            loading === true ?
-                                                <ActivityIndicator animating={true} color="#7878C1" />
-                                                :
-                                                <VictoryPie
-                                                    data={data}
-                                                    colorScale={finalColors}
-                                                    padAngle={1}
-                                                    innerRadius={40}
-                                                    radius={({ index }) => index === selectedSlice ? 170 : 150}
-                                                    width={350}
-                                                    height={369}
-                                                    labelRadius={({ innerRadius }) => innerRadius + 65}
-                                                    events={[{
-                                                        target: "data",
-                                                        eventHandlers: {
-                                                            onPressIn: handleSliceClick,
-                                                        },
-                                                    }]}
-                                                    labelComponent={<VictoryLabel style={{
-                                                        fontFamily: 'Lexend_400Regular',
-                                                        fontSize: 18,
-                                                        fontWeight: 500
-                                                    }} />}
-                                                />
-                                        }
-                                    </View>
-                                    );
-                                };
+    const handleSliceClick = (event, props) => {
+        setSelectedSlice(selectedSlice === props.index ? null : props.index);
+    };
 
-                                    const styles = StyleSheet.create({
-                                        container: {
-                                        width: 328,
-                                    height: 356,
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                        },
-                                    });
+    //Label Font. This is connected to the app.json "font" object.
+    const [fontsLoaded] = useFonts({
+        Lexend_400Regular,
+    });
 
-                                    export default DonutChart;
+    return (
+        <View style={[styles.container, globalStyles.labelText]}>
+            {
+                loading === true ?
+                    <ActivityIndicator animating={true} color="#7878C1" />
+                    :
+                    <VictoryPie
+                        data={data}
+                        colorScale={finalColors}
+                        padAngle={1}
+                        innerRadius={40}
+                        radius={({ index }) => index === selectedSlice ? 170 : 150}
+                        width={350}
+                        height={369}
+                        labelRadius={({ innerRadius }) => innerRadius + 65}
+                        events={[{
+                            target: "data",
+                            eventHandlers: {
+                                onPressIn: handleSliceClick,
+                            },
+                        }]}
+                        labelComponent={<VictoryLabel style={{
+                            fontFamily: 'Lexend_400Regular',
+                            fontSize: 18,
+                            fontWeight: 500
+                        }} />}
+                    />
+            }
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        width: 328,
+        height: 356,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default DonutChart;
                                     `}
                                 </code>
                             </pre>
@@ -561,24 +554,18 @@ import { ActivityIndicator } from 'react-native-paper';
                         <div className={styles.solMiddle}>
                             <div className={styles.solPadding}>
                                 <Image
-                                    src={''}
-                                    alt=''
-                                    height={361}
-                                    width={345}
-                                />
-                            </div>
-                            <div className={styles.solLessPadding}>
-                                <Image
-                                    src={''}
-                                    alt=''
-                                    height={361}
-                                    width={345}
+                                    className={styles.imgPers}
+                                    src={'/images/innersight/solScreens.png'}
+                                    alt='Tech Screens'
+                                    height={450}
+                                    width={700}
                                 />
                             </div>
                             <div className={styles.solMWText}>
                                 <Image
-                                    src={''}
-                                    alt=''
+                                    className={styles.imgPers}
+                                    src={'/images/innersight/solutionHand.png'}
+                                    alt='Hand with screens'
                                     height={361}
                                     width={345}
                                 />
